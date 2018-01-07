9fea4e4c4b7720287656fe50c0eb94953291bdd6
水谷 太祐 <dancing_company@mbp.local>
2018-01-07 16:18:22 +0900

    CreateHeart

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'dengonban', to: 'dengonban#index'
+  post 'dengonban', to: 'dengonban#index'
+  get 'dengonban/index'
+  post 'dengonban/index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
